576bf1310255893ca6ff70ff764ef3012532a1fa
emhamza <imumairhamza@gmail.com>
2023-06-02 04:49:13 -0700

    Add Hero section

---
+++ src/components/Introduce/Introduce.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import "./Introduce.scss";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
+
+const Introduce = () => {
+  return (
+    <div>
+        <section>
+            <div>
+                <div>
+                    <h4 className="circular-border">
+                        <FontAwesomeIcon icon={faHome} /> INTRODUCE
+                    </h4>
+                </div>
+            </div>
+        </section>
+    </div>
+  )
+}
+
+export default Introduce;
