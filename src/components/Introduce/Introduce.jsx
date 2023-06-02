063c9154b301e8a55d4241f3ddf83c45a7898c63
emhamza <imumairhamza@gmail.com>
2023-06-02 05:21:12 -0700

    Add hero section

--- src/components/Introduce/Introduce.jsx
+++ src/components/Introduce/Introduce.jsx
@@ -5,12 +5,13 @@ import { faHome } from '@fortawesome/free-solid-svg-icons';
 const Introduce = () => {
   return (
     <div>
-        <section>
+        <section className="d-flex">
             <div>
-                <div>
+                <div className="hero-header">
                     <h4 className="circular-border">
                         <FontAwesomeIcon icon={faHome} /> INTRODUCE
                     </h4>
+                    <h1>Say Hi from <span className="hamza"> Hamza</span>, Webapp Designer and Developer</h1>
                 </div>
             </div>
         </section>
