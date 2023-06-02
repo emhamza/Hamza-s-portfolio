1390930527e9eafad3410d65470ec56ca4b46bda
emhamza <imumairhamza@gmail.com>
2023-06-02 05:00:03 -0700

    Add Hero section

--- src/components/Introduce/Introduce.jsx
+++ src/components/Introduce/Introduce.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import "./Introduce.scss";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome } from '@fortawesome/free-solid-svg-icons';
